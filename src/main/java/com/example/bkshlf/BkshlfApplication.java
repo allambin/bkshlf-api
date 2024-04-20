4f52d3083ab0f32160e15ed05863f28d5abc99c2
Anne-Lise Lambin <annelise.lambin@gmail.com>
2024-04-20 12:40:37 +0100

    init

---
+++ src/main/java/com/example/bkshlf/BkshlfApplication.java
@@ -0,0 +1,13 @@
+package com.example.bkshlf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkshlfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkshlfApplication.class, args);
+	}
+
+}
